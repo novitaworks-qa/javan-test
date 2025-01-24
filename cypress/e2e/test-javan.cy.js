671769609e52fd96d5b01755d4539fed107a08ca
Novita <novita@Zakkys-Mac-mini.local>
2025-01-24 16:05:40 +0700

    update

--- cypress/e2e/test-javan.cy.js
+++ cypress/e2e/test-javan.cy.js
@@ -21,26 +21,13 @@ describe('Check Broken Links', () => {
 describe('Form Submission - Error Popup', () => {
   it('should show error popup after submitting form with invalid email, but not error for number in name', () => {
     cy.visit('https://javan.co.id/contact')
-    
-    // Mengisi form dengan angka pada nama
-    cy.get('input[name="name"]').type('12345')  // Mengisi nama dengan angka
-    cy.get('input[name="nickname"]').type('John')
-    
-    // Mengisi email yang tidak valid
-    cy.get('input[name="email"]').first().type('invalid@email') // Memilih elemen pertama
-    
-    cy.get('input[name="phone"]').type('086789000111')
+    cy.get('input[name="name"]').type('12345') 
+    cy.get('input[name="email"]').first().type('invalid@email') 
     cy.get('input[name="institution"]').type('jaya abadi')
-    cy.get('select[name="subject"]').select('Ads')  // Ubah klik menjadi select
+    cy.get('select[name="subject"]').select('Ads') 
     cy.get('textarea[name="message"]').type('johndooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooo')
-
-    // Kirimkan formulir
     cy.get('button[type="submit"]').click()
-
-    // Pastikan popup dengan pesan kesalahan email yang invalid muncul
     cy.get('.popup-message').should('be.visible').and('contain', 'Please enter a valid email address')
-
-    // Pastikan tidak ada kesalahan terkait dengan nama yang berisi angka
     cy.get('.popup-message').should('not.contain', 'Please enter a valid name')
   })
 })
